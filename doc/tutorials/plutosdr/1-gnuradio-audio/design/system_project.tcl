ee998a295dfd6b3a2871e257c0973841201d30dc
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2019-03-06 21:47:06 +0100

    doc:tutorials:plutosdr:1-gnuradio-audio: fix path in TCL

--- doc/tutorials/plutosdr/1-gnuradio-audio/design/system_project.tcl
+++ doc/tutorials/plutosdr/1-gnuradio-audio/design/system_project.tcl
@@ -1,5 +1,5 @@
-
-source /home/gwe/boards/pluto/plutosdr-fw/hdl/projects/scripts/adi_env.tcl
+variable adi_hdl_dir $::env(ADI_HDL_DIR)
+source $adi_hdl_dir/projects/scripts/adi_env.tcl
 source $ad_hdl_dir/projects/scripts/adi_project.tcl
 source $ad_hdl_dir/projects/scripts/adi_board.tcl
 
@@ -10,10 +10,6 @@ adi_project_files pluto [list \
   "system_top.v" \
   "system_constr.xdc" \
   "$ad_hdl_dir/library/xilinx/common/ad_iobuf.v"]
-
-variable fpga_ip    $::env(GGM_FPGA_IP)
-set_property  ip_repo_paths  [list ${fpga_ip} ${ad_hdl_dir}/library] [current_project]
-update_ip_catalog
 
 set_property is_enabled false [get_files  *system_sys_ps7_0.xdc]
 adi_project_run pluto
