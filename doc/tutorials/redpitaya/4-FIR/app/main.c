7923547a339d9156211fc57ed2e94abc34896fca
jmfriedt <jean-michel.friedt@femto-st.fr>
2019-04-13 16:29:39 +0200

    header file

--- doc/tutorials/redpitaya/4-FIR/app/main.c
+++ doc/tutorials/redpitaya/4-FIR/app/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <fcntl.h>
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include "fir_conf.h" // library for communicating with the FIR
